4e1300cd2776abb29b329266397fe48c4b8bd90d
ajssmith <ansmith@redhat.com>
2019-02-27 10:25:11 -0500

    Update launch.sh
    Update to configmap template generated by qdrouterd operator

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+export HOSTNAME_IP_ADDRESS=$(hostname -i)
+
 CONFIG_FILE=${QDROUTERD_HOME}/etc/qdrouterd.conf
+CONFIG_MAP_FILE=/etc/qpid-dispatch/qdrouterd.conf.template
+
+if [ -f $CONFIG_MAP_FILE ]; then
+    DOLLAR='$' envsubst < $CONFIG_MAP_FILE > $CONFIG_FILE
+fi
 
 ${QDROUTERD_HOME}/bin/configure.sh ${QDROUTERD_HOME} $CONFIG_FILE
 
