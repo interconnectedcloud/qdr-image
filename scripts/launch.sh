c6a57eb175e5302b9eaa24205589eff05aa6cb35
ajssmith <ansmith@redhat.com>
2019-02-27 13:47:25 -0500

    Update launch.sh

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -3,11 +3,6 @@
 export HOSTNAME_IP_ADDRESS=$(hostname -i)
 
 CONFIG_FILE=${QDROUTERD_HOME}/etc/qdrouterd.conf
-CONFIG_MAP_FILE=/etc/qpid-dispatch/qdrouterd.conf.template
-
-if [ -f $CONFIG_MAP_FILE ]; then
-    DOLLAR='$' envsubst < $CONFIG_MAP_FILE > $CONFIG_FILE
-fi
 
 ${QDROUTERD_HOME}/bin/configure.sh ${QDROUTERD_HOME} $CONFIG_FILE
 
