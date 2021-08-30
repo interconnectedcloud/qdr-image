64833e395bdd6378f62bae9fad199c43db38a283
Fernando Giorgetti <fgiorget@redhat.com>
2021-08-30 09:38:55 -0300

    Fixed nightly condition to avoid failures on first job

--- .github/scripts/github-changed-24h.sh
+++ .github/scripts/github-changed-24h.sh
@@ -11,12 +11,11 @@ LAST_COMMIT_TIMESTAMP=`date -u -d "${LAST_COMMIT}" +%s`
 NOW_TIMESTAMP=`date -u +%s`
 ELAPSED_SECS=$((NOW_TIMESTAMP - LAST_COMMIT_TIMESTAMP))
 
-RC=0
 if [[ ${ELAPSED_SECS} -lt 86400 ]]; then
     echo "Last commit happened in the past 24h: ${LAST_COMMIT}"
+    echo "::set-output name=changed::true"
 else
     echo "Last commit is past 24h: ${LAST_COMMIT}"
-    RC=1
+    echo "::set-output name=changed::false"
 fi
 echo "${SHA} - ${AUTHOR} - ${MSG}"
-exit $RC
